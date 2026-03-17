cc77af7bf7cd8be2760907841ccbd75ed1dbdf2d
Lucho <bravo.luis.1995@gmail.com>
2026-03-17 14:02:59 -0500

    fix cors

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -54,7 +54,7 @@ public class SecurityConfig {
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration config = new CorsConfiguration();
     config.setAllowedOrigins(
-        List.of("http://localhost:4200", "https://reinoso-cs6ieo0c9-luiisao20s-projects.vercel.app"));
+        List.of("http://localhost:4200", "https://reinoso-app.vercel.app/"));
     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     config.setAllowedHeaders(List.of("*"));
     config.setAllowCredentials(true);
